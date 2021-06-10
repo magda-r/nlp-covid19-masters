47e8bd4d91ea5c90a693c5832f0e79ad426e48c7
magda-r <rac.magdalena@gmail.com>
2021-06-10 12:11:40 +0200

    Create twitter_data.py

---
+++ notebooks/twitter_data.py
@@ -0,0 +1,31 @@
+# %%
+import os
+import tweepy as tw
+import pandas as pd
+
+# %%
+consumer_key= 'yourkeyhere'
+consumer_secret= 'yourkeyhere'
+access_token= 'yourkeyhere'
+access_token_secret= 'yourkeyhere'
+
+# %%
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+# %%
+# Define the search term and the date_since date as variables
+search_words = "#wildfires"
+date_since = "2018-11-16"
+
+# Collect tweets
+tweets = tw.Cursor(api.search,
+              q=search_words,
+              lang="en",
+              since=date_since).items(5)
+tweets
+
+# %%
+# Collect a list of tweets
+[tweet.text for tweet in tweets]
